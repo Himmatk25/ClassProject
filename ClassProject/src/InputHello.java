3e33989b88f926cc6dc43d5dd0acc6698ca06d97
himmat.khangura1 <himmat.khangura1@zone36-wc.cs.ucalgary.ca>
2019-07-15 17:05:30 -0600

    hi himmat

--- ClassProject/src/InputHello.java
+++ ClassProject/src/InputHello.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class InputHello{
 
   public static void main(String[] args) {
-      System.out.println("What is your name?");
+      System.out.println("What is your name???");
       Scanner userinput = new Scanner(System.in);
       String name = userinput.nextLine();
       System.out.println("Hello" + name);
